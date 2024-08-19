47772ba8f6c04702645d3d94291c335909e7e992
Edsamn <eduardomsribas@gmail.comm>
2024-08-19 13:07:33 -0300

    commit feat: tweets controller e route, patch: user controller

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import usersRoutes from "./routes/users.routes";
+import tweetsRoutes from "./routes/tweets.routes";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes());
+app.use("/tweets", tweetsRoutes());
 
 const port = process.env.PORT;
 
